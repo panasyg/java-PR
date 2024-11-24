f13141e712dce6a1e4b33ff8609ab08b7d9cc804
Johnny <91308486+panasyg@users.noreply.github.com>
2024-11-24 11:38:42 +0200

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "Zdayemo kursach eeeuu!";
 	}
 
 //	echo "# github-actions-example" >> README.md
